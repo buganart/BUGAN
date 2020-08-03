b457b04339a4f694adc907f62cc6e3c6b26e70dc
sveitser <sveitser@gmail.com>
2020-08-03 12:01:57 +0800

    Don't re-process or overwrite by default

--- convert3d/convert3d/__main__.py
+++ convert3d/convert3d/__main__.py
@@ -16,13 +16,17 @@ def convert(input_path, output_path, voxel_size):
 @click.option("--output-dir", "-o", required=True)
 @click.option("--voxel-size", "-s", type=float, default=1.0)
 @click.option("--n-jobs", "-n", type=int, default=-1)
-def main(input_dir, output_dir, voxel_size, n_jobs):
+@click.option("--overwrite", "-w", is_flag=True)
+def main(input_dir, output_dir, voxel_size, n_jobs, overwrite):
 
     input_paths = list(Path(input_dir).rglob("*.*"))
     output_paths = [
         Path(output_dir).joinpath(input_path.relative_to(input_dir)).with_suffix(".ply")
         for input_path in input_paths
     ]
+
+    if not overwrite:
+        output_paths = [path for path in output_paths if not path.exists()]
 
     dirs = set([path.parent for path in output_paths])
     for directory in dirs:
