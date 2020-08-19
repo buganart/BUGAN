33607ba4b5153d073e4d2f2ec8a55a6021a72f4f
Ryan Au <ryau123@gmail.com>
2020-08-20 00:15:45 +0800

    upload refactored code that process dataset and generate model

--- bugan/models.py
+++ bugan/models.py
@@ -1,2 +1,290 @@
-class AwesomeModel:
-    pass
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device
+
+#####
+#   models for training
+#####
+class VAEGAN(nn.Module):
+    def __init__(self, vae_encoder_layer =1, vae_decoder_layer = 2, z_size = 128, d_layer = 1):
+        super(VAEGAN, self).__init__()
+        self.z_size = z_size
+        #VAE
+        self.VAE = VAE(vae_encoder_layer, vae_decoder_layer, z_size)
+        #GAN
+        self.discriminator = Discriminator(d_layer)
+
+    def forward(self, x):
+        #VAE
+        x = self.VAE(x)
+        #classifier and discriminator
+        x = self.discriminator(x)
+        return x
+
+    def generate_tree(self, check_D = False, num_trees = 1, num_try = 100, config = None):
+        #num_try is number of trial to generate a tree that can fool D
+        #total number of sample generated = num_trees * num_try
+        vae = self.VAE.to(device)
+        discriminator = self.discriminator.to(device)
+
+        result = None
+
+        if config is None:
+            batch_size = 4
+        else:
+            batch_size = config.batch_size
+
+        if not check_D:
+            num_tree_total = num_trees
+            num_runs = int(np.ceil(num_tree_total / batch_size))
+            #ignore discriminator
+            for i in range(num_runs):
+                #generate noise vector
+                z = torch.randn(batch_size, vae.z_size).float().to(device)
+                tree_fake = vae.generate_sample(z)[:,0,:,:,:]
+                selected_trees = tree_fake.detach().cpu().numpy()
+                if result is None:
+                    result = selected_trees
+                else:
+                    result = np.concatenate((result, selected_trees), axis=0)
+        else:
+            num_tree_total = num_trees * num_try
+            num_runs = int(np.ceil(num_tree_total / batch_size))
+            #only show samples can fool discriminator
+            for i in range(num_runs):
+                #generate noise vector
+                z = torch.randn(batch_size, vae.z_size).float().to(device)
+                
+                tree_fake = vae.generate_sample(z)
+                dout, _ = discriminator(tree_fake)
+                dout = dout > 0.5
+                selected_trees = tree_fake[dout].detach().cpu().numpy()
+                if result is None:
+                    result = selected_trees
+                else:
+                    result = np.concatenate((result, selected_trees), axis=0)
+        #select at most num_trees
+        if result.shape[0] > num_trees:
+            result = result[:num_trees]
+        #in case no good result
+        if result.shape[0] <= 0:
+            result = np.zeros((1,64,64,64))
+            result[0,0,0,0] = 1
+        return result
+
+
+class GAN(nn.Module):
+    def __init__(self, g_layer = 2, d_layer = 1, input_channel=128):
+        super(GAN, self).__init__()
+        self.generator = Generator(g_layer, input_channel)
+        self.discriminator = Discriminator(d_layer)
+
+    def forward(self, x):
+        x = self.generator(x)
+        x = self.discriminator(x)
+        return x
+
+    def generate_tree(self, check_D = False, num_trees = 1, num_try = 100, config = None):
+        #num_try is number of trial to generate a tree that can fool D
+        #total number of sample generated = num_trees * num_try
+        
+        generator = self.generator.to(device)
+        discriminator = self.discriminator.to(device)
+
+        result = None
+
+        if config is None:
+            batch_size = 4
+        else:
+            batch_size = config.batch_size
+
+        if not check_D:
+            num_tree_total = num_trees
+            num_runs = int(np.ceil(num_tree_total / batch_size))
+            #ignore discriminator
+            for i in range(num_runs):
+                #generate noise vector
+                z = torch.randn(batch_size, 128).to(device)
+                
+                tree_fake = generator(z)[:,0,:,:,:]
+                selected_trees = tree_fake.detach().cpu().numpy()
+                if result is None:
+                    result = selected_trees
+                else:
+                    result = np.concatenate((result, selected_trees), axis=0)
+        else:
+            num_tree_total = num_trees * num_try
+            num_runs = int(np.ceil(num_tree_total / batch_size))
+            #only show samples can fool discriminator
+            for i in range(num_runs):
+                #generate noise vector
+                z = torch.randn(batch_size, 128).to(device)
+                
+                tree_fake = generator(z)
+                dout, _ = discriminator(tree_fake)
+                dout = dout > 0.5
+                selected_trees = tree_fake[dout].detach().cpu().numpy()
+                if result is None:
+                    result = selected_trees
+                else:
+                    result = np.concatenate((result, selected_trees), axis=0)
+        #select at most num_trees
+        if result.shape[0] > num_trees:
+            result = result[:num_trees]
+        #in case no good result
+        if result.shape[0] <= 0:
+            result = np.zeros((1,64,64,64))
+            result[0,0,0,0] = 1
+        return result
+
+#####
+#   building blocks for models 
+#####
+
+class Generator(nn.Module):
+    def __init__(self, layer_per_block=1, input_channel=128):
+        super(Generator, self).__init__()
+        self.input_channel = input_channel
+
+        #layer_per_block must be >= 1
+        if layer_per_block < 1:
+            layer_per_block = 1
+
+        self.fc_channel = 8 #16
+        self.fc_size = 4
+
+        num_unit1 = self.fc_channel   
+        num_unit2 = 16   #32
+        num_unit3 = 32   #64
+        num_unit4 = 64   #128
+        num_unit5 = 32   #256
+        num_unit6 = 16   #512
+
+        num_layer_unit_list = [num_unit1, num_unit2, num_unit3, num_unit4, num_unit5, num_unit6]
+        gen_module = []
+        #5 blocks (need 4 pool to reduce size)
+        for i in range(5):
+            num_layer_unit1, num_layer_unit2 = num_layer_unit_list[i], num_layer_unit_list[i+1]
+
+            gen_module.append(nn.ConvTranspose3d(num_layer_unit1, num_layer_unit2, 3, 1, padding = 1))
+            gen_module.append(nn.BatchNorm3d(num_layer_unit2))
+            gen_module.append(nn.ReLU(True))
+
+            for _ in range(layer_per_block):
+                gen_module.append(nn.ConvTranspose3d(num_layer_unit2, num_layer_unit2, 3, 1, padding = 1))
+                gen_module.append(nn.BatchNorm3d(num_layer_unit2))
+                gen_module.append(nn.ReLU(True))
+
+            gen_module.append(nn.Upsample(scale_factor=2, mode='trilinear'))
+
+        #remove extra pool layer
+        gen_module = gen_module[:-1]
+
+        #add final sigmoid 
+        gen_module.append(nn.ConvTranspose3d(num_unit6, 1, 3, 1, padding = 1))
+        gen_module.append(nn.Sigmoid())
+
+        
+
+        self.gen_fc = nn.Linear(self.input_channel, num_unit1 * self.fc_size * self.fc_size * self.fc_size)
+        self.gen = nn.Sequential(*gen_module)
+
+    def forward(self, x):
+        x = self.gen_fc(x)
+        x = x.view(x.shape[0], self.fc_channel, self.fc_size, self.fc_size, self.fc_size)
+        x = self.gen(x)
+        return x
+
+
+class Discriminator(nn.Module):
+    def __init__(self, layer_per_block=1, final_fc_size = 128):
+        super(Discriminator, self).__init__()
+
+         #layer_per_block must be >= 1
+        if layer_per_block < 1:
+            layer_per_block = 1
+
+        num_unit1 = 1   #input channel number
+        num_unit2 = 8   #32
+        num_unit3 = 16   #64
+        num_unit4 = 32  #128
+        num_unit5 = 16   #256
+        num_unit6 = 8   #512
+
+
+        num_layer_unit_list = [num_unit1, num_unit2, num_unit3, num_unit4, num_unit5, num_unit6]
+        dis_module = []
+        #5 blocks (need 4 pool to reduce size)
+        for i in range(5):
+            num_layer_unit1, num_layer_unit2 = num_layer_unit_list[i], num_layer_unit_list[i+1]
+
+            dis_module.append(nn.Conv3d(num_layer_unit1, num_layer_unit2, 3, 1, padding = 1))
+            dis_module.append(nn.BatchNorm3d(num_layer_unit2))
+            dis_module.append(nn.ReLU(True))
+
+            for _ in range(layer_per_block):
+                dis_module.append(nn.Conv3d(num_layer_unit2, num_layer_unit2, 3, 1, padding = 1))
+                dis_module.append(nn.BatchNorm3d(num_layer_unit2))
+                dis_module.append(nn.ReLU(True))
+
+            dis_module.append(nn.MaxPool3d((2, 2, 2)))
+
+        #remove extra pool layer
+        dis_module = dis_module[:-1]
+
+        
+        self.dis = nn.Sequential(*dis_module)
+
+        self.dis_fc1 = nn.Sequential(
+            nn.Linear(num_unit6 * 4 * 4 * 4, final_fc_size),
+            nn.ReLU(True)
+        )
+        self.dis_fc2 = nn.Sequential(
+            nn.Linear(final_fc_size, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+
+        x = self.dis(x)
+        x = x.view(x.shape[0], -1)
+        fx = self.dis_fc1(x)
+        x = self.dis_fc2(fx)
+        return x, fx
+
+class VAE(nn.Module):
+    def __init__(self, vae_encoder_layer = 1, vae_decoder_layer = 2, z_size = 128):
+        super(VAE, self).__init__()
+        self.z_size = z_size
+        #VAE
+        self.vae_encoder = Discriminator(vae_encoder_layer, z_size)
+        self.encoder_mean = nn.Linear(z_size, z_size)
+        self.encoder_logvar = nn.Linear(z_size, z_size)
+        self.vae_decoder = Generator(vae_decoder_layer, z_size)
+
+
+    #reference: https://github.com/YixinChen-AI/CVAE-GAN-zoos-PyTorch-Beginner/blob/master/CVAE-GAN/CVAE-GAN.py
+    def noise_reparameterize(self,mean,logvar):
+        eps = torch.randn(mean.shape).to(device)
+        z = mean + eps * torch.exp(logvar)
+        return z
+
+    def forward(self, x):
+        #VAE
+        _, f = self.vae_encoder(x)
+        x_mean = self.encoder_mean(f)
+        x_logvar = self.encoder_logvar(f)
+        x = self.noise_reparameterize(x_mean, x_logvar)
+        x = self.vae_decoder(x)
+        return x
+
+    def generate_sample(self, z):
+        x = self.vae_decoder(z)
+        return x
+
