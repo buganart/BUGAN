ff1c26ec70f3335a48def41518b6bbea901e8c2f
Ryan Au <ryau123@gmail.com>
2020-09-07 13:31:18 +0800

    changed BCELoss to BCEwithLogit

--- bugan/modelsPL.py
+++ bugan/modelsPL.py
@@ -107,8 +107,8 @@ class VAEGAN(pl.LightningModule):
         discriminator = self.discriminator
 
         #loss function
-        criterion_label = nn.BCELoss
-        criterion_reconstruct = nn.MSELoss
+        criterion_label = nn.BCEWithLogitsLoss
+        criterion_reconstruct = nn.BCEWithLogitsLoss
         criterion_label = criterion_label(reduction='mean')
             
         #labels
@@ -146,7 +146,7 @@ class VAEGAN(pl.LightningModule):
             vae_rec_loss += KL
 
             #output of the vae should fool discriminator
-            vae_out_d = discriminator(reconstructed_data)
+            vae_out_d = discriminator(F.sigmoid(reconstructed_data))
             vae_d_loss = criterion_label(vae_out_d, real_label)
 
             vae_loss = (vae_recon_loss_factor * vae_rec_loss + vae_d_loss) / (vae_recon_loss_factor + 1)   #scale the loss to one
@@ -167,7 +167,7 @@ class VAEGAN(pl.LightningModule):
             #generate fake trees
             latent_size = vae.decoder_z_size
             z = torch.randn(batch_size, latent_size).float().to(device) #noise vector
-            tree_fake = vae.generate_sample(z)
+            tree_fake = F.sigmoid(vae.generate_sample(z))
 
             #fake data (data from generator) 
             dout_fake = discriminator(tree_fake.clone().detach())   #detach so no update to generator
@@ -207,7 +207,7 @@ class VAEGAN(pl.LightningModule):
             for i in range(num_runs):
                 #generate noise vector
                 z = torch.randn(batch_size, vae.decoder_z_size).float().to(device)
-                tree_fake = vae.generate_sample(z)[:,0,:,:,:]
+                tree_fake = F.sigmoid(vae.generate_sample(z))[:,0,:,:,:]
                 selected_trees = tree_fake.detach().cpu().numpy()
                 if result is None:
                     result = selected_trees
@@ -221,8 +221,8 @@ class VAEGAN(pl.LightningModule):
                 #generate noise vector
                 z = torch.randn(batch_size, vae.decoder_z_size).float().to(device)
                 
-                tree_fake = vae.generate_sample(z)
-                dout, _ = discriminator(tree_fake)
+                tree_fake = F.sigmoid(vae.generate_sample(z))
+                dout = F.sigmoid(discriminator(tree_fake))
                 dout = dout > 0.5
                 selected_trees = tree_fake[dout].detach().cpu().numpy()
                 if result is None:
@@ -239,114 +239,204 @@ class VAEGAN(pl.LightningModule):
         return result
 
 
-# class GAN(nn.Module):
-#     def __init__(self, generator, discriminator):
-#         super(GAN, self).__init__()
-#         assert(generator.output_size == discriminator.input_size)
 
-#         self.generator = generator
-#         self.discriminator = discriminator
+class GAN(pl.LightningModule):
+    def __init__(self, config, trainer, save_model_path):
+        super(GAN, self).__init__()
 
-#     def forward(self, x):
-#         x = self.generator(x)
-#         x = self.discriminator(x)
-#         return x
+        self.config = config
+        self.trainer = trainer
+        self.save_model_path = save_model_path
+        #create components
+        generator = Generator(config.g_layer, config.z_size, config.array_size, config.gen_num_layer_unit)
 
-    
-#     def compute_loss(self, dataset_batch, criterion_label = nn.BCELoss(reduction='mean') ):
-#     # this function calculate loss of the model given a dataset_batch from dataloader    
-#         #loss function
-#         # criterion = lambda input,target : nn.BCELoss(reduction='none')(input,target).mean()    #mean per element in each data, mean over batch 
-#         # criterion = nn.BCELoss(reduction='mean')    #sum over element in each sample, mean over batch
+        discriminator = Discriminator(config.d_layer, config.z_size, config.array_size, config.dis_num_layer_unit)
 
-#         batch_size = dataset_batch.shape[0]
-            
-#         #labels
-#         real_label = torch.unsqueeze(torch.ones(batch_size),1).float().to(device)
-#         fake_label = torch.unsqueeze(torch.zeros(batch_size),1).float().to(device)
+        #GAN
+        self.generator = generator
+        self.discriminator = discriminator
 
-#         ############
-#         #   discriminator
-#         ############
-#         #generate fake trees
-#         z = torch.randn(batch_size, 128).float().to(device) #128-d noise vector
-#         tree_fake = self.generator(z)
+        #for logging
+        self.d_ep_loss = 0.
+        self.g_ep_loss = 0.
+        self.epoch = 0
 
-#         #real data (data from dataloader)
-#         dout_real, features_real = self.discriminator(dataset_batch, output_all=True)
-#         dloss_real = criterion_label(dout_real, real_label)
-#         score_real = dout_real
-#         #fake data (data from generator)            
-#         dout_fake = self.discriminator(tree_fake.clone().detach())   #detach so no update to generator
-#         dloss_fake = criterion_label(dout_fake, fake_label)
-#         score_fake = dout_fake
-
-#         #loss function (discriminator classify real data vs generated data)
-#         dloss = (dloss_real + dloss_fake)/2
-
-#         ############
-#         #   generator
-#         ############
-
-#         #tree_fake is already computed above
-#         dout_fake, features_fake = self.discriminator(tree_fake, output_all=True)
-#         #generator should generate trees that discriminator think they are real
-#         gloss = criterion_label(dout_fake, real_label)
-
-#         return dloss, gloss
-
-#     def generate_tree(self, check_D = False, num_trees = 1, num_try = 100, config = None):
-#         #num_try is number of trial to generate a tree that can fool D
-#         #total number of sample generated = num_trees * num_try
         
-#         generator = self.generator.to(device)
-#         discriminator = self.discriminator.to(device)
+    def forward(self, x):
+        #classifier and discriminator
+        x = self.generator(x)
+        x = F.sigmoid(x)
+        x = self.discriminator(x)
+        return x
 
-#         result = None
+    def configure_optimizers(self):
+        config = self.config
+        generator = self.generator
+        discriminator = self.discriminator
 
-#         if config is None:
-#             batch_size = 4
-#         else:
-#             batch_size = config.batch_size
+        #optimizer
+        if config.gen_opt == "Adam":
+            self.generator_optimizer = optim.Adam(generator.parameters(), lr=config.g_lr)
+        else:
+            self.generator_optimizer = optim.SGD(generator.parameters(), lr=config.g_lr)
 
-#         if not check_D:
-#             num_tree_total = num_trees
-#             num_runs = int(np.ceil(num_tree_total / batch_size))
-#             #ignore discriminator
-#             for i in range(num_runs):
-#                 #generate noise vector
-#                 z = torch.randn(batch_size, 128).to(device)
+        if config.dis_opt == "Adam":
+            self.discriminator_optimizer = optim.Adam(discriminator.parameters(), lr=config.d_lr)
+        else:
+            self.discriminator_optimizer = optim.SGD(discriminator.parameters(), lr=config.d_lr)
+
+        return self.generator_optimizer, self.discriminator_optimizer
+
+    def on_train_epoch_start(self):
+        #reset ep_loss
+        self.d_ep_loss = 0.
+        self.g_ep_loss = 0.
+
+        #set model to train
+        self.generator.train()
+        self.discriminator.train()
+
+    def on_train_epoch_end(self):
+        self.d_ep_loss = self.d_ep_loss / self.config.num_data
+        self.g_ep_loss = self.g_ep_loss / self.config.num_data
+
+        #save model if necessary
+        log_dict = {
+            "discriminator loss": self.d_ep_loss,
+            "generator loss": self.g_ep_loss}
+
+        log_image = (self.epoch % self.config.log_image_interval == 0)    #boolean whether to log image
+        log_mesh = (self.epoch % self.config.log_mesh_interval == 0)    #boolean whether to log mesh
+
+        wandbLog(self, log_dict, log_image=log_image, log_mesh=log_mesh) 
+        if log_image:
+            self.trainer.save_checkpoint(self.save_model_path)
+            save_checkpoint_to_cloud(self.save_model_path)
+        self.epoch += 1
+
+
+    def training_step(self, dataset_batch, batch_idx, optimizer_idx):
+        config = self.config
+
+        dataset_batch = dataset_batch[0]    #dataset_batch was a list: [array], so just take the array inside
+        dataset_batch = dataset_batch.float().to(device)
+
+        batch_size = dataset_batch.shape[0]
+        generator = self.generator
+        discriminator = self.discriminator
+
+        #loss function
+        criterion_label = nn.BCEWithLogitsLoss
+        criterion_label = criterion_label(reduction='mean')
+            
+        #labels
+        real_label = torch.unsqueeze(torch.ones(batch_size),1).float().to(device)
+        fake_label = torch.unsqueeze(torch.zeros(batch_size),1).float().to(device)
+
+
+        if optimizer_idx == 0:
+            ############
+            #   generator
+            ############
+
+            z = torch.randn(batch_size, config.z_size).float().to(device) #128-d noise vector
+            tree_fake = F.sigmoid(self.generator(z))
+           
+            #tree_fake is already computed above
+            dout_fake = self.discriminator(tree_fake, output_all=False)
+            #generator should generate trees that discriminator think they are real
+            gloss = criterion_label(dout_fake, real_label)
+
+            #record loss
+            self.g_ep_loss += gloss.detach()
+
+            result = pl.TrainResult(minimize=gloss, checkpoint_on=gloss)
+            result.log('g_loss', gloss, on_epoch=True, prog_bar=True)
+            return result
+
+        if optimizer_idx == 1:
+
+            ############
+            #   discriminator (and classifier if necessary)
+            ############
+            
+            z = torch.randn(batch_size, config.z_size).float().to(device) #128-d noise vector
+            tree_fake = F.sigmoid(self.generator(z))
+
+            #real data (data from dataloader)
+            dout_real = self.discriminator(dataset_batch, output_all=False)
+            dloss_real = criterion_label(dout_real, real_label)
+            score_real = dout_real
+            #fake data (data from generator)            
+            dout_fake = self.discriminator(tree_fake.clone().detach())   #detach so no update to generator
+            dloss_fake = criterion_label(dout_fake, fake_label)
+            score_fake = dout_fake
+
+            #loss function (discriminator classify real data vs generated data)
+            dloss = (dloss_real + dloss_fake)/2
+
+            #record loss
+            self.d_ep_loss += dloss.detach()  
+
+            result = pl.TrainResult(minimize=dloss)
+            result.log('dloss', dloss, on_epoch=True, prog_bar=True)
+            return result
+
+
+    def generate_tree(self, check_D = False, num_trees = 1, num_try = 100):
+        #num_try is number of trial to generate a tree that can fool D
+        #total number of sample generated = num_trees * num_try
+        config = self.config
+        generator = self.generator.to(device)
+        discriminator = self.discriminator.to(device)
+
+        result = None
+
+        if config is None:
+            batch_size = 4
+        else:
+            batch_size = config.batch_size
+
+        if not check_D:
+            num_tree_total = num_trees
+            num_runs = int(np.ceil(num_tree_total / batch_size))
+            #ignore discriminator
+            for i in range(num_runs):
+                #generate noise vector
+                z = torch.randn(batch_size, 128).to(device)
                 
-#                 tree_fake = generator(z)[:,0,:,:,:]
-#                 selected_trees = tree_fake.detach().cpu().numpy()
-#                 if result is None:
-#                     result = selected_trees
-#                 else:
-#                     result = np.concatenate((result, selected_trees), axis=0)
-#         else:
-#             num_tree_total = num_trees * num_try
-#             num_runs = int(np.ceil(num_tree_total / batch_size))
-#             #only show samples can fool discriminator
-#             for i in range(num_runs):
-#                 #generate noise vector
-#                 z = torch.randn(batch_size, 128).to(device)
+                tree_fake = generator(z)[:,0,:,:,:]
+                selected_trees = tree_fake.detach().cpu().numpy()
+                if result is None:
+                    result = selected_trees
+                else:
+                    result = np.concatenate((result, selected_trees), axis=0)
+        else:
+            num_tree_total = num_trees * num_try
+            num_runs = int(np.ceil(num_tree_total / batch_size))
+            #only show samples can fool discriminator
+            for i in range(num_runs):
+                #generate noise vector
+                z = torch.randn(batch_size, 128).to(device)
                 
-#                 tree_fake = generator(z)
-#                 dout = discriminator(tree_fake)
-#                 dout = dout > 0.5
-#                 selected_trees = tree_fake[dout].detach().cpu().numpy()
-#                 if result is None:
-#                     result = selected_trees
-#                 else:
-#                     result = np.concatenate((result, selected_trees), axis=0)
-#         #select at most num_trees
-#         if result.shape[0] > num_trees:
-#             result = result[:num_trees]
-#         #in case no good result
-#         if result.shape[0] <= 0:
-#             result = np.zeros((1,64,64,64))
-#             result[0,0,0,0] = 1
-#         return result
+                tree_fake = generator(z)
+                dout = F.sigmoid(discriminator(tree_fake))
+                dout = dout > 0.5
+                selected_trees = tree_fake[dout].detach().cpu().numpy()
+                if result is None:
+                    result = selected_trees
+                else:
+                    result = np.concatenate((result, selected_trees), axis=0)
+        #select at most num_trees
+        if result.shape[0] > num_trees:
+            result = result[:num_trees]
+        #in case no good result
+        if result.shape[0] <= 0:
+            result = np.zeros((1,64,64,64))
+            result[0,0,0,0] = 1
+        return result
+
+
 
 #####
 #   building blocks for models 
@@ -400,9 +490,9 @@ class Generator(nn.Module):
         #remove extra pool layer
         gen_module = gen_module[:-1]
 
-        #add final sigmoid 
+        #remove sigmoid for loss with logit
         gen_module.append(nn.ConvTranspose3d(num_layer_unit_list[-1], 1, 3, 1, padding = 1))
-        gen_module.append(nn.Sigmoid())
+        # gen_module.append(nn.Sigmoid())   
 
         
 
@@ -472,7 +562,7 @@ class Discriminator(nn.Module):
         )
         self.dis_fc2 = nn.Sequential(
             nn.Linear(z_size, 1),
-            nn.Sigmoid()
+            # nn.Sigmoid()  #remove sigmoid for loss with logit
         )
 
     def forward(self, x, output_all=False):
