e2a76527a674b730b4c23ff8eaabc9ae7db69c6d
sveitser <sveitser@gmail.com>
2020-11-12 12:25:11 +0800

    Test dataloader with zip file

--- tests/test_data_loader.py
+++ tests/test_data_loader.py
@@ -1,14 +1,27 @@
 from argparse import Namespace
 from pathlib import Path
+import shutil
+import zipfile
 
 import pytest
 
 from bugan.functionsPL import DataModule_process
 
 
+@pytest.fixture(params=["zip", "folder"])
+def data_path(request, tmp_path):
+    data_folder = Path(__file__).parent / "data"
+    if request.param == "folder":
+        tmp_data_path = shutil.copytree(data_folder, tmp_path / "data")
+        return tmp_data_path
+    else:
+        zip_path = shutil.make_archive(tmp_path / "dataset", "zip", data_folder)
+        return zip_path
+
+
 @pytest.mark.parametrize("data_augmentation", [True, False])
 @pytest.mark.parametrize("rotation_type", ["random rotation", "axis rotation"])
-def test_data_module_folder(data_augmentation, rotation_type):
+def test_data_module_folder(data_path, data_augmentation, rotation_type):
 
     config = Namespace(
         batch_size=1,
@@ -17,7 +30,6 @@ def test_data_module_folder(data_augmentation, rotation_type):
         aug_rotation_type="random rotation",
     )
 
-    data_path = Path(__file__).parent / "data"
     data_module = DataModule_process(config, run=None, data_path=data_path)
     data_module.prepare_data()
     data_module.setup()
