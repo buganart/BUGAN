d4988431157f7641ae4948adb3efac78c6304de0
sveitser <sveitser@gmail.com>
2020-11-13 20:42:50 +0800

    Revert "Colab notebooks do not capture bugan ..."
    This reverts commit 86a5fe04e66e2d73c3c634c0fbcaeb46e53f1f30.

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bugan",
+    version="0.0.1",
     url="https://github.com/buganart/BUGAN",
     author="buganart",
     description="3D GANs and more.",
@@ -19,6 +20,4 @@ setup(
         "scipy",
         "wandb",
     ],
-    version_format="{tag}.dev{commitcount}+{gitsha}",
-    setup_requires=["setuptools-git-version"],
 )
