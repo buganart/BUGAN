a69df5dab2e240d78362f82534b61271275c4c8b
Ryan Au <ryau123@gmail.com>
2020-12-14 17:35:00 +0800

    added spectral_norm to generator and discriminator

--- bugan/modelsPL.py
+++ bugan/modelsPL.py
@@ -46,6 +46,8 @@ class VAE_train(pl.LightningModule):
             "--linear_annealed_instance_noise_epoch", type=int, default=2000
         )
         parser.add_argument("--instance_noise", type=float, default=0.1)
+        # spectral_norm
+        parser.add_argument("--spectral_norm", type=bool, default=False)
         # learning rate
         parser.add_argument("--vae_lr", type=float, default=0.0025)
         # number of unit per layer
@@ -79,6 +81,7 @@ class VAE_train(pl.LightningModule):
             config.resolution,
             config.decoder_num_layer_unit,
             dropout_prob=config.dropout_prob,
+            spectral_norm=config.spectral_norm,
             activations=nn.LeakyReLU(config.activation_leakyReLU_slope, True),
         )
         encoder = Discriminator(
@@ -86,6 +89,8 @@ class VAE_train(pl.LightningModule):
             config.z_size,
             config.resolution,
             config.encoder_num_layer_unit,
+            dropout_prob=config.dropout_prob,
+            spectral_norm=config.spectral_norm,
             activations=nn.LeakyReLU(config.activation_leakyReLU_slope, True),
         )
         vae = VAE(encoder=encoder, decoder=decoder)
@@ -255,6 +260,8 @@ class VAEGAN(pl.LightningModule):
             "--linear_annealed_instance_noise_epoch", type=int, default=2000
         )
         parser.add_argument("--instance_noise", type=float, default=0.1)
+        # spectral_norm
+        parser.add_argument("--spectral_norm", type=bool, default=False)
         # learning rate
         parser.add_argument("--vae_lr", type=float, default=0.0025)
         parser.add_argument("--d_lr", type=float, default=0.00005)
@@ -292,6 +299,7 @@ class VAEGAN(pl.LightningModule):
             config.resolution,
             config.decoder_num_layer_unit,
             dropout_prob=config.dropout_prob,
+            spectral_norm=config.spectral_norm,
             activations=nn.LeakyReLU(config.activation_leakyReLU_slope, True),
         )
         encoder = Discriminator(
@@ -299,6 +307,8 @@ class VAEGAN(pl.LightningModule):
             config.z_size,
             config.resolution,
             config.encoder_num_layer_unit,
+            dropout_prob=config.dropout_prob,
+            spectral_norm=config.spectral_norm,
             activations=nn.LeakyReLU(config.activation_leakyReLU_slope, True),
         )
         vae = VAE(encoder=encoder, decoder=decoder)
@@ -308,6 +318,8 @@ class VAEGAN(pl.LightningModule):
             config.z_size,
             config.resolution,
             config.dis_num_layer_unit,
+            dropout_prob=config.dropout_prob,
+            spectral_norm=config.spectral_norm,
             activations=nn.LeakyReLU(config.activation_leakyReLU_slope, True),
         )
 
@@ -561,6 +573,8 @@ class GAN(pl.LightningModule):
             "--linear_annealed_instance_noise_epoch", type=int, default=2000
         )
         parser.add_argument("--instance_noise", type=float, default=0.1)
+        # spectral_norm
+        parser.add_argument("--spectral_norm", type=bool, default=False)
         # learning rate
         parser.add_argument("--g_lr", type=float, default=0.0025)
         parser.add_argument("--d_lr", type=float, default=0.00005)
@@ -595,6 +609,7 @@ class GAN(pl.LightningModule):
             config.resolution,
             config.gen_num_layer_unit,
             dropout_prob=config.dropout_prob,
+            spectral_norm=config.spectral_norm,
             activations=nn.LeakyReLU(config.activation_leakyReLU_slope, True),
         )
 
@@ -603,6 +618,8 @@ class GAN(pl.LightningModule):
             config.z_size,
             config.resolution,
             config.dis_num_layer_unit,
+            dropout_prob=config.dropout_prob,
+            spectral_norm=config.spectral_norm,
             activations=nn.LeakyReLU(config.activation_leakyReLU_slope, True),
         )
 
@@ -1253,6 +1270,7 @@ class CGAN(GAN):
             config.resolution,
             config.gen_num_layer_unit,
             dropout_prob=config.dropout_prob,
+            spectral_norm=config.spectral_norm,
             activations=nn.LeakyReLU(config.activation_leakyReLU_slope, True),
         )
 
@@ -1261,6 +1279,8 @@ class CGAN(GAN):
             config.z_size,
             config.resolution,
             config.dis_num_layer_unit,
+            dropout_prob=config.dropout_prob,
+            spectral_norm=config.spectral_norm,
             activations=nn.LeakyReLU(config.activation_leakyReLU_slope, True),
         )
 
@@ -1270,6 +1290,8 @@ class CGAN(GAN):
             config.resolution,
             config.dis_num_layer_unit,
             output_size=config.num_classes,
+            dropout_prob=config.dropout_prob,
+            spectral_norm=config.spectral_norm,
             activations=nn.LeakyReLU(config.activation_leakyReLU_slope, True),
         )
 
@@ -1588,6 +1610,7 @@ class Generator(nn.Module):
         output_size=64,
         num_layer_unit=32,
         dropout_prob=0.3,
+        spectral_norm=False,
         activations=nn.ReLU(True),
     ):
         super(Generator, self).__init__()
@@ -1629,22 +1652,25 @@ class Generator(nn.Module):
                 num_layer_unit_list[i + 1],
             )
 
-            gen_module.append(
-                nn.ConvTranspose3d(num_layer_unit1, num_layer_unit2, 3, 1, padding=1)
-            )
-            gen_module.append(nn.BatchNorm3d(num_layer_unit2))
-            gen_module.append(activations)
-            gen_module.append(nn.Dropout3d(dropout_prob))
-
-            for _ in range(layer_per_block - 1):
-                gen_module.append(
-                    nn.ConvTranspose3d(
-                        num_layer_unit2, num_layer_unit2, 3, 1, padding=1
+            for _ in range(layer_per_block):
+                if spectral_norm:
+                    gen_module.append(
+                        SpectralNorm(
+                            nn.ConvTranspose3d(
+                                num_layer_unit1, num_layer_unit2, 3, 1, padding=1
+                            )
+                        )
                     )
-                )
+                else:
+                    gen_module.append(
+                        nn.ConvTranspose3d(
+                            num_layer_unit1, num_layer_unit2, 3, 1, padding=1
+                        )
+                    )
                 gen_module.append(nn.BatchNorm3d(num_layer_unit2))
                 gen_module.append(activations)
                 gen_module.append(nn.Dropout3d(dropout_prob))
+                num_layer_unit1 = num_layer_unit2
 
             gen_module.append(nn.Upsample(scale_factor=2, mode="trilinear"))
 
@@ -1652,9 +1678,16 @@ class Generator(nn.Module):
         gen_module = gen_module[:-1]
 
         # remove tanh for loss with logit
-        gen_module.append(
-            nn.ConvTranspose3d(num_layer_unit_list[-1], 1, 3, 1, padding=1)
-        )
+        if spectral_norm:
+            gen_module.append(
+                SpectralNorm(
+                    nn.ConvTranspose3d(num_layer_unit_list[-1], 1, 3, 1, padding=1)
+                )
+            )
+        else:
+            gen_module.append(
+                nn.ConvTranspose3d(num_layer_unit_list[-1], 1, 3, 1, padding=1)
+            )
         # gen_module.append(nn.tanh())
 
         self.gen_fc = nn.Linear(
@@ -1680,6 +1713,7 @@ class Discriminator(nn.Module):
         num_layer_unit=16,
         dropout_prob=0.3,
         output_size=1,
+        spectral_norm=False,
         activations=nn.LeakyReLU(0.0, True),
     ):
         super(Discriminator, self).__init__()
@@ -1722,18 +1756,21 @@ class Discriminator(nn.Module):
                 num_layer_unit_list[i + 1],
             )
 
-            dis_module.append(
-                nn.Conv3d(num_layer_unit1, num_layer_unit2, 3, 1, padding=1)
-            )
-            dis_module.append(nn.BatchNorm3d(num_layer_unit2))
-            dis_module.append(activations)
-
-            for _ in range(layer_per_block - 1):
-                dis_module.append(
-                    nn.Conv3d(num_layer_unit2, num_layer_unit2, 3, 1, padding=1)
-                )
+            for _ in range(layer_per_block):
+                if spectral_norm:
+                    dis_module.append(
+                        SpectralNorm(
+                            nn.Conv3d(num_layer_unit1, num_layer_unit2, 3, 1, padding=1)
+                        )
+                    )
+                else:
+                    dis_module.append(
+                        nn.Conv3d(num_layer_unit1, num_layer_unit2, 3, 1, padding=1)
+                    )
                 dis_module.append(nn.BatchNorm3d(num_layer_unit2))
                 dis_module.append(activations)
+                dis_module.append(nn.Dropout3d(dropout_prob))
+                num_layer_unit1 = num_layer_unit2
 
             dis_module.append(nn.MaxPool3d((2, 2, 2)))
 
@@ -1856,6 +1893,73 @@ class CVAE(nn.Module):
 
         x = self.vae_decoder(z)
         return x
+
+
+###
+#       other modules
+###
+
+#
+#   reference: https://github.com/heykeetae/Self-Attention-GAN/blob/master/spectral.py
+#
+class SpectralNorm(nn.Module):
+    def __init__(self, module, name="weight", power_iterations=1):
+        super(SpectralNorm, self).__init__()
+        self.module = module
+        self.name = name
+        self.power_iterations = power_iterations
+        if not self._made_params():
+            self._make_params()
+
+    def l2normalize(self, v, eps=1e-12):
+        return v / (v.norm() + eps)
+
+    def _update_u_v(self):
+        u = getattr(self.module, self.name + "_u")
+        v = getattr(self.module, self.name + "_v")
+        w = getattr(self.module, self.name + "_bar")
+
+        height = w.data.shape[0]
+        for _ in range(self.power_iterations):
+            v.data = self.l2normalize(
+                torch.mv(torch.t(w.view(height, -1).data), u.data)
+            )
+            u.data = self.l2normalize(torch.mv(w.view(height, -1).data, v.data))
+
+        # sigma = torch.dot(u.data, torch.mv(w.view(height,-1).data, v.data))
+        sigma = u.dot(w.view(height, -1).mv(v))
+        setattr(self.module, self.name, w / sigma.expand_as(w))
+
+    def _made_params(self):
+        try:
+            u = getattr(self.module, self.name + "_u")
+            v = getattr(self.module, self.name + "_v")
+            w = getattr(self.module, self.name + "_bar")
+            return True
+        except AttributeError:
+            return False
+
+    def _make_params(self):
+        w = getattr(self.module, self.name)
+
+        height = w.data.shape[0]
+        width = w.view(height, -1).data.shape[1]
+
+        u = nn.Parameter(w.data.new(height).normal_(0, 1), requires_grad=False)
+        v = nn.Parameter(w.data.new(width).normal_(0, 1), requires_grad=False)
+        u.data = self.l2normalize(u.data)
+        v.data = self.l2normalize(v.data)
+        w_bar = nn.Parameter(w.data)
+
+        del self.module._parameters[self.name]
+
+        self.module.register_parameter(self.name + "_u", u)
+        self.module.register_parameter(self.name + "_v", v)
+        self.module.register_parameter(self.name + "_bar", w_bar)
+
+    def forward(self, *args):
+        self._update_u_v()
+        return self.module.forward(*args)
 
 
 ###
