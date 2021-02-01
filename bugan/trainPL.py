8f5d6d3d904af9c5e9d5d745af2030cefe1db545
Ryan Au <ryau123@gmail.com>
2021-02-01 15:53:13 +0800

    update trainPL to enable Wloss_GP models

--- bugan/trainPL.py
+++ bugan/trainPL.py
@@ -53,8 +53,11 @@ from bugan.modelsPL import (
     GAN,
     GAN_Wloss,
     GAN_Wloss_GP,
+    VAEGAN_Wloss_GP,
     CGAN,
     CVAEGAN,
+    CGAN_Wloss_GP,
+    CVAEGAN_Wloss_GP,
 )
 
 
@@ -69,10 +72,16 @@ def _get_models(model_name):
         MODEL_CLASS = GAN_Wloss
     elif model_name == "WGAN_GP":
         MODEL_CLASS = GAN_Wloss_GP
+    elif model_name == "VAEGAN_GP":
+        MODEL_CLASS = VAEGAN_Wloss_GP
     elif model_name == "CGAN":
         MODEL_CLASS = CGAN
-    else:
+    elif model_name == "CVAEGAN":
         MODEL_CLASS = CVAEGAN
+    elif model_name == "CGAN_GP":
+        MODEL_CLASS = CGAN_Wloss_GP
+    else:
+        MODEL_CLASS = CVAEGAN_Wloss_GP
     return MODEL_CLASS
 
 
@@ -257,7 +266,14 @@ def main():
     else:
         config.dataset = "dataset_array_custom"
     # adjust parameter datatype
-    if config.selected_model in ["VAEGAN", "GAN", "VAE", "WGAN", "WGAN_GP"]:
+    if config.selected_model in [
+        "VAEGAN",
+        "GAN",
+        "VAE",
+        "WGAN",
+        "WGAN_GP",
+        "VAEGAN_GP",
+    ]:
         config.num_classes = 0
 
     # run offline
